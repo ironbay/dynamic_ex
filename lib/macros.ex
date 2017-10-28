44e1c79f4d84b3d9e31ad9829d49ac5d788a44ba
Dax Ravi <d@ironbay.co>
2017-10-27 20:47:10 -0400

    Dynamic macros

---
+++ lib/macros.ex
@@ -0,0 +1,36 @@
+defmodule Dynamic.Macros do
+
+	defmacro get(fun) do
+		quote do
+			get(unquote(fun), Atom.to_string(unquote(fun)))
+		end
+	end
+	defmacro get(fun, path) do
+		quote do
+			get(unquote(fun), unquote(path), &(&1))
+		end
+	end
+
+	defmacro get(fun, path, cb) when is_list(path) === false do
+		quote do
+			get(unquote(fun), [unquote(path)], unquote(cb))
+		end
+	end
+	defmacro get(fun, path, cb) do
+		quote do
+			def unquote(fun)(input) do
+				result =
+					input
+					|> Dynamic.get(unquote(path))
+				unquote(cb).(result)
+			end
+		end
+	end
+end
+
+defmodule Dynamic.Example do
+	import Dynamic.Macros
+
+	get :patient
+	get :nice
+end
